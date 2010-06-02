65de9e7820de794e3e8be0626387ea40862b57a7
whitered <dmitry.zhelnin@gmail.com>
2010-06-02 12:16:55 +0400

    rakefile added

---
+++ rakefile.rb
@@ -0,0 +1,16 @@
+require 'sprout'
+
+sprout 'as3'
+
+task :default => 'kote.swc'
+
+
+
+compc 'kote.swc' do |t|
+  source_dir = 'src'
+  t.output = 'bin/kote.swc'
+  t.source_path << source_dir
+  t.source_path << File.join('libs', 'signaller', 'src')
+  t.static_link_runtime_shared_libraries = true
+  t.include_classes << Dir.chdir(source_dir) { Dir.glob(File.join('**', '*.as'))}.map { |fn| fn.gsub('.as', '').gsub(File::SEPARATOR, '.') }
+end
