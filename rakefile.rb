5adbb6e83a7eef66fa61a282f1cf23fcb53e35e7
whitered <dmitry.zhelnin@gmail.com>
2010-09-27 16:10:43 +0400

    rake downloads signaller library before building the swc

--- rakefile.rb
+++ rakefile.rb
@@ -5,7 +5,7 @@ sprout 'as3'
 task :default => :build
 
 desc 'Build the swc package'
-task :build => 'kote.swc'
+task :build => ['libs:signaller:fetch', 'kote.swc']
 
 
 
@@ -16,4 +16,17 @@ compc 'kote.swc' do |t|
   t.source_path << File.join('libs', 'signaller', 'src')
   t.static_link_runtime_shared_libraries = true
   t.include_classes << Dir.chdir(source_dir) { Dir.glob(File.join('**', '*.as'))}.map { |fn| fn.gsub('.as', '').gsub(File::SEPARATOR, '.') }
+end
+
+
+
+namespace :libs do
+  namespace :signaller do
+
+    desc 'Fetch signaller sources from github'
+    task :fetch do
+      system 'git submodule init'
+      system 'git submodule update'
+    end
+  end
 end
