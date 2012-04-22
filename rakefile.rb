9987eef5b940958fc076a4a06a3c13d4ca0ce79a
whitered <dmitry.zhelnin@gmail.com>
2012-04-23 02:15:21 +0400

    update rakefile, add Gemfile

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,15 @@
-require 'sprout'
+require 'rubygems'
+require 'bundler'
+require 'bundler/setup'
 
-sprout 'as3'
+require 'rake/clean'
+require 'flashsdk'
+require 'asunit4'
 
 task :default => :build
 
 desc 'Build the swc package'
 task :build => ['libs:signaller:fetch', 'kote.swc']
-
-
 
 compc 'kote.swc' do |t|
   source_dir = 'src'
@@ -17,8 +19,6 @@ compc 'kote.swc' do |t|
   t.static_link_runtime_shared_libraries = true
   t.include_classes << Dir.chdir(source_dir) { Dir.glob(File.join('**', '*.as'))}.map { |fn| fn.gsub('.as', '').gsub(File::SEPARATOR, '.') }
 end
-
-
 
 namespace :libs do
   namespace :signaller do
